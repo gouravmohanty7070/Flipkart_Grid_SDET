2a6d30e5e2453167a9d485b9c770c04d694553a1
goyalpramod <goyalpramod1729@gmail.com>
2023-08-16 13:57:33 +0530

    improve outfit generator

--- LLM.py
+++ LLM.py
@@ -26,20 +26,40 @@ prompt = ChatPromptTemplate(
         ),
         # The `variable_name` here is what must align with memory
         HumanMessagePromptTemplate.from_template(
-            """You are used for help in choosing clothing and accesories for fashion 
-                You will be given a text that you need to convert into a query
-                Only convert it into words that can be used to describe clothing or fashion accessories
-                example 1: "I want a blue shirt for my brother"
-                output: blue shirt for man 
-                example 2: "I want a kurta for diwali"
-                output: festive kurta
-                example 3: "Something in stylish black shirt"
-                output: stylish black shirt
-                IMPORTANT if you get ambiguous ideas like date night, festivals etc. Use your knowledge to discern what kind of clothes will be best for that occasion and give that as output
-                example 1: "Festival clothing for christmas"
-                output: christmas clothing
-                example 2: "Date night clothes"
-                output: black slim dress
+            f"""You are a fashion outfit generator, You will be given user details, user past purchase, user frequent purchase inside <context> </context> and what is the idea or kind of outfit the user is looking for inside <wish> </wish>. Generate a personalized recommendation of out fit for the user. 
+                In the output first give a good description of the outfit and how it will suit with your wish, then a DICTIONARY output of the outfit,  if a field is not required in the DICTIONARY write none there.
+
+                "topwear" : "...", "bottomwear":"...",  "footwear": "...", "accesories": "..."
+
+                Give fashionable and coherent recommendations, that go together with each other
+
+                the user may tell you they like part of the suggestion and some part not, only change the part that they mention they did not like and keep rest of the suggestion same
+
+                only recommend one item in each category
+
+                Below is an example 
+
+                My input:
+                <context>
+                user details : 30 year old man living in india with 36 waist size and 40 shirt size.
+                user past purchase : black flat shirt, lewis jeans, oxford shoes 
+                user frequent purchase : hats
+                </context>
+
+                <wish>
+                I have a formal meeting with my boss, what should I wear
+                </wish>
+
+                Your output:
+
+                A formal meeting will go well with formal wear. It will be good if you wear something that makes you look....
+
+                <OUTFIT>
+
+                "topwear": "Blue formal shirt", "bottomwear" : "black chinos", "footwear": "formal shoes", "accesories" : "Analog watch"
+
+                </OUTFIT>
+                IMPORTANT Do not reply with "as an ai model.." under any circumstances
                                                  """),
         MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{question}")
@@ -60,3 +80,22 @@ conversation = LLMChain(
 
 def get_query_from_text(question):
     return conversation.run({"question": question})
+
+wish = """
+<context>
+user details: 34 year old married man with brown skin 
+user past purchase: blue jeans, polo shirt 
+user frequent purchase: none
+</context>
+
+<wish>
+I have my anniversary night with my wife, I want to wear something nice
+</wish>
+
+"""
+
+print(get_query_from_text(wish))
+
+# wish_2 = "slim fit clothes make me look fat, tell me something else"
+
+# print(get_query_from_text(wish_2))
